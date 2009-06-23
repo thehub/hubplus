4831af27f502a9f84a94c42f0fdd039476604b9c
phil <phil.jones@the-hub.net>
2009-06-23 15:50:52 +0200

    support in views for editing individual profile fields

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^username_autocomplete/$', 'misc.views.username_autocomplete_friends', name='profile_username_autocomplete'),
     url(r'^$', 'profiles.views.profiles', name='profile_list'),
     url(r'^(?P<username>[\w\._-]+)/$', 'profiles.views.profile', name='profile_detail'),
+    url(r'^(?P<username>[\w\._-]+)/field/(?P<fieldname>[\w_]+)/$','profiles.views.profile_field',name='profile_field'),
 )
