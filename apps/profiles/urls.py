08bdf03ea0110e29925dfc70c21d1457c09550cd
phil <phil.jones@the-hub.net>
2009-06-24 15:33:57 +0200

    decorator for permission testing profile access, starting working with django.ModelForms

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^(?P<username>[\w\._-]+)/field/(?P<fieldname>[\w_]+)/$','profiles.views.profile_field',{"class":"Profile"},name='profile_field'),
     url(r'^(?P<username>[\w\._-]+)/host_info/field/(?P<fieldname>[\w_]+)/$','profiles.views.profile_field',{"class":"HostInfo"},name='host_info_profile_field'),
     url(r'^(?P<username>[\w\._-]+)/interests/$','profiles.views.add_profile_interest',name='profile_interests'),
-
+    url(r'^(?P<username>[\w\._-]+)/skills/$','profiles.views.add_profile_skill',name='profile_skills'),
+    url(r'^(?P<username>[\w\._-]+)/needs/$','profiles.views.add_profile_need',name='profile_needs'),
 
 )
