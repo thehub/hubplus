1990ec6fc93c9ad258e972169674baca0c44b32b
phil <phil.jones@the-hub.net>
2009-07-10 20:23:15 +0200

    generalizing the classname argument passed to field_editing

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -268,9 +268,9 @@ def profile_field(request,username,classname,fieldname,*args,**kwargs) :
     if not ps.has_access(request.user,p,ps.get_interface_id(Profile,'Editor')) :
         return HttpResponse("You aren't authorized to access %s in %s for %s. You are %s" % (fieldname,kwargs['class'],username,request.user),status=401)
     else :
-        if kwargs['class'] == 'Profile' :
+        if classname == 'Profile' :
             return one_model_field(request,p,ProfileForm,fieldname, kwargs.get('default', ''),[p.user])
-        elif kwargs['class'] == 'HostInfo' :
+        elif classname == 'HostInfo' :
             return one_model_field(request,p.get_host_info(),HostInfoForm,fieldname, kwargs.get('default', ''),[p.user])
 
 
