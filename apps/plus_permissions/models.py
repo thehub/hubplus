94c3d3f7634def2767f24c755771ee7d671eec8d
salfield <salfield@hubplus.the-hub.net>
2009-08-16 14:43:35 +0100

    the foreign key referencing explicit security context must be unique

--- apps/plus_permissions/models.py
+++ apps/plus_permissions/models.py
@@ -115,8 +115,8 @@ class GenericReference(models.Model):
     obj = generic.GenericForeignKey()
     
     acquires_from = models.ForeignKey("GenericReference", related_name="acquirers", null=True)
-    acquired_scontext =  models.ForeignKey(SecurityContext, related_name="controlled", null=True)
-    explicit_scontext = models.ForeignKey(SecurityContext, related_name="target", null=True)
+    acquired_scontext = models.ForeignKey(SecurityContext, related_name="controlled", null=True)
+    explicit_scontext = models.ForeignKey(SecurityContext, related_name="target", null=True, unique=True)
 
 
 
