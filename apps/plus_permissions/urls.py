999a9bb408d606c84b9a92a5d5c39fbe9ec176ec
salfield <salfield@hubplus.the-hub.net>
2009-08-27 12:41:14 +0100

    group security patch for hubspace

--- apps/plus_permissions/urls.py
+++ apps/plus_permissions/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
-                       url(r'^patch_in_profiles$', 'plus_permissions.views.patch_in_profiles', name='profile_list')
+                       url(r'^patch_in_profiles$', 'plus_permissions.views.patch_in_profiles', name='hubspace_profile_patch'),
+                       url(r'^patch_in_groups$', 'plus_permissions.views.patch_in_groups', name='hubspace_group_patch')
                        )
--- apps/plus_permissions/views.py
+++ apps/plus_permissions/views.py
@@ -10,16 +10,60 @@ from django.db import transaction
 from django.utils import simplejson
 
 from django.contrib.auth.decorators import login_required
+from plus_permissions.types.TgGroup import setup_group_security
+from apps.plus_permissions.permissionable import create_reference
+from apps.plus_permissions.default_agents import get_all_members_group, get_or_create_root_location, get_admin_user
+from apps.plus_permissions.types.User import setup_user_security
+from apps.hubspace_compatibility.models import TgGroup
 
+from django.contrib.auth.models import User
+
+
+
+def setup_hubs_security(group, creator):
+    """creator will be admin user
+    """
+    if not creator:
+        raise TypeError("We must have a creator to create a group, since otherwise it will be inaccessible")
+
+    create_reference(TgGroup, group)
+    if group.level == 'member':
+        admin_group = TgGroup.objects.get(place=group.place, level='host')
+        setup_hubs_security(admin_group, creator)
+        setup_group_security(group, group, admin_group, creator)
+    elif group.level == 'host':
+        setup_group_security(group, group, group, creator)
+    print `group`
+    return group
+
+@login_required
+def patch_in_groups(request):
+    """Do group security setup for hubspace groups
+    XXX patch group names to reflect Location names
+    XXX ensure all directors are in the host group
+    1. setup a security context for each group passing in context_agent, context_admin and creator
+    2. set 'hosts' as a members of the members group, ignore 'directors' groups (they are deprecated - bring on host anarchy!)
+    """
+
+    no_security = [group for group in TgGroup.objects.filter(level='member').exclude(place__name='hubplus') if not group.ref.all()]
+    admin_user = get_admin_user()
+    for group in no_security:
+        setup_hubs_security(group, admin_user)
+    return HttpResponse("patched %s hub group's security" % str(len(no_security)))
 
 @login_required
 def patch_in_profiles(request):
-    """create profiles for all users who don't have them
+    """create profiles and setup security hubspace users
     """
-    from apps.plus_permissions.permissionable import create_reference
-    from apps.plus_permissions.types.User import setup_user_security
 
-    from django.contrib.auth.models import User
+    site_members_group = get_all_members_group()
+    site_members = site_members_group.users.all()
+
+    users = User.objects.all()
+    for user in users:
+        if user not in site_members:
+            site_members_group.users.add(user)
+
     users = User.objects.filter(profile__isnull=True)
     no_of_users = users.count()
 
