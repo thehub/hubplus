ee66fd98a499b4e02aa7d06b468d2e85b2f9f9ea
salfield <salfield@hubplus.the-hub.net>
2009-09-03 21:55:57 +0100

    sliders with initial levels set

--- site_media/js/plus_permissions/views.js
+++ site_media/js/plus_permissions/views.js
@@ -6,14 +6,10 @@ var initTabView = function (ele) {
 var permission_ready = function () {
     var all_sliders = jq('#permission_sliders');
     initTabView(all_sliders.get(0));
-    var Event = YAHOO.util.Event;
-    var Dom   = YAHOO.util.Dom;
-    var lang  = YAHOO.lang;
-    var Y     = YAHOO.util.Selector;
 
     jq('.permission_button').overlay({expose: {
-            color: '#BAD0DB',
-            opacity: 0.7
+            color: '#000000',
+            opacity: 0.5
         }});
     jq('.permission_button').click(function () {
 	var resource_id= jq(this).attr('id').split('-')[1];
@@ -23,7 +19,15 @@ var permission_ready = function () {
 	var custom_default = "/permission/toggle_custom";
 	jq.getJSON(load_url, function(json){
 	    jq('#overlay_content').html(jq(json.html));
-	    var sliders = json.sliders;
+	    var sliders = {};
+	    jq.each(json.sliders, function(index, slider_set){
+		sliders[slider_set[0]] = slider_set[1];
+		var interface_dict = {};
+		jq.each(sliders[slider_set[0]]['interface_levels'], function(index, slider_levels) {
+		    interface_dict[slider_levels[0]] = slider_levels[1];
+		});
+		sliders[slider_set[0]]['interface_levels'] = interface_dict;
+	    });
 	    var agents = json.agents;
 	    var custom = json.custom;
 	    var create_slider_points = function (tbody) {
@@ -40,6 +44,7 @@ var permission_ready = function () {
 	    };
 	    var setup_slider_group = function (i, ele) {
 		var slider_group = jq(ele);
+		var obj_class = slider_group.attr('id').split('-')[0];
 		var tbody = slider_group.find('tbody');
 		var heights = create_slider_points(tbody);
 		slider_group.find('.slider_holder').each( function (i, ele) {
@@ -48,6 +53,9 @@ var permission_ready = function () {
 		    var top_cell = jq(ele).parent();
 		    var bottom = tbody.height() - 13 - (jq(ele).parent().offset().top - tbody.offset().top);
 		    var slider = YAHOO.widget.Slider.getVertSlider(ele.id, jq(ele).find('.slider').get(0), top, bottom);
+		    var level_agent = sliders[obj_class]['interface_levels'][_interface.split('_')[1]];
+		    var row = jq('#agent-{class}-{id}'.supplant({'class':level_agent.classname, 'id':level_agent.id}));
+		    slider.setValue(row.data('slider').middle);
 		    slider.subscribe("change", function(offsetFromStart) {
 			var s_cells = jq('.' + _interface);
 			s_cells.each(function (i, cell) {
@@ -85,23 +93,3 @@ var permission_ready = function () {
     });
 };
 
-/*
-
-	slider.subscribe("change",function(offsetFromStart) {
-		scaled = Math.round(scale(this.getValue()));
-		slider_model.set_current(scaled);
-		$('#scratch').html(sg.titles[slider_model.id] +
-	                   " ("+slider_model.min+"), "+scaled+', '+sg.option_labels[scaled] );
-
-		if (scaled != slider_model.get_current()) {
-		    slider.setValue(scale.rev(slider_model.get_current()));
-		}
-	    });
-
-	slider.callback = function(slider_model) {
-	    // this lets the slider_model call us back to update constraints
-	    $('#scratch2').html('model changed '+sg.titles[slider_model.id]+" min: "+slider_model.min);
-	    this.update_position(slider_model);
-	};
-
-*/
