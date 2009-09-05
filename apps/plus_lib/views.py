c7caabc0556e41db3a82c05a559a4977a25766c5
phil <phil.jones@the-hub.net>
2009-09-05 19:00:30 +0200

    tweaks

--- apps/plus_lib/views.py
+++ apps/plus_lib/views.py
@@ -16,16 +16,16 @@ validation_mapping = {
 def inner_objects(x) :
     # Some object fields are proxies for another dependent class which also needs to be saved
     # eg. some Profile attributes are really User attributes and we must save the User object when we change them     
-    # inner objects now takes an object and returns a function which returns a list of the inner objects    
+    # inner objects now takes an object and returns a list of the inner objects    
     if x.__class__ == 'Profile' : 
         return [x.user]
-    # we may add more here if there 
+    # we may add more here if we find some
     return []
-
+ 
 @secure_resource(obj_schema={'object':'any'})
 def field(request, object, fieldname, default='') :
     """This should be the generic "attribute" editor ... for any normal attribute, 
-    this function lets use get its value, (with a default if it has none) or update it"""
+    this function lets us get its value, (with a default if it has none) or update it"""
     
     val = getattr(object, fieldname)
     if not request.POST:
@@ -34,7 +34,6 @@ def field(request, object, fieldname, default='') :
         return HttpResponse("%s" % val, mimetype="text/plain")
 
     new_val = request.POST['value']
-
 
     if validation_mapping.has_key(object.__class__.__name__) :
         # there's a form class which may validate this field
@@ -50,11 +49,10 @@ def field(request, object, fieldname, default='') :
         setattr(object, fieldname, new_val)
         object.save()
 
-        if inner_objects.has_key :
-            # if some attributes of this object are, in fact, mere proxies for another object
-            # then we must save that object too. Classic example : Profile and Users. Some Profile 
-            # attributes now delegated to User
-            for o in inner_objects :
+        # if some attributes of this object are, in fact, mere proxies for another object
+        # then we must save that object too. Classic example : Profile and Users. Some Profile 
+        # attributes now delegated to User
+        for o in inner_objects(object) :
                 o.save()
     except Exception, e :
         return HttpResponse('%s' % e, status=500)
