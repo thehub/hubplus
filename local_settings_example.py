3bb12cd89bdacdf968d94b12704826928d2e741d
phil <phil.jones@the-hub.net>
2009-09-07 16:01:37 +0200

    example local_settings

--- local_settings_example.py
+++ local_settings_example.py
@@ -1,7 +1,7 @@
 DATABASE_ENGINE = 'postgresql_psycopg2'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'another_hubplus'       # Or path to database file if using sqlite3.
-DATABASE_USER = 'thehub'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'pwfth'         # Not used with sqlite3. 
+DATABASE_NAME = 'DBNAME'       # Or path to database file if using sqlite3.
+DATABASE_USER = 'DBUSER'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'DBPASSWORD'         # Not used with sqlite3. 
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
