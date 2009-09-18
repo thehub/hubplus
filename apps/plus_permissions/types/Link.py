a454e4c614aff52f827797504c391f69477df236
phil <phil.jones@the-hub.net>
2009-09-18 18:52:02 +0200

    type

---
+++ apps/plus_permissions/types/Link.py
@@ -0,0 +1,27 @@
+from apps.plus_permissions.interfaces import InterfaceReadProperty, InterfaceWriteProperty, InterfaceCallProperty, InterfaceReadWriteProperty
+from apps.plus_permissions.models import SetSliderOptions, SetAgentDefaults, SetPossibleTypes, SetSliderAgents
+from apps.plus_links.models import Link
+
+content_type = Link
+
+
+class LinkViewer :
+    text = InterfaceReadProperty
+    url = InterfaceReadProperty
+    target = InterfaceReadProperty
+    service = InterfaceReadProperty
+
+class LinkManager :
+    delete = InterfaceCallProperty
+
+from apps.plus_permissions.models import add_type_to_interface_map
+LinkInterfaces = {'Viewer': LinkViewer,  'Manager' : LinkManager  }
+add_type_to_interface_map(Link, LinkInterfaces)
+
+
+SliderOptions = {'InterfaceOrder':['LinkViewer']}
+SetSliderOptions(Link, SliderOptions)
+
+child_types = []
+SetPossibleTypes(Link, child_types)
+
