5e20be9963581f38a0c09ae526d155424b89b471
phil <phil.jones@the-hub.net>
2009-10-09 04:23:11 +0200

    main_hub_name

--- apps/profiles/templatetags/profile_tags.py
+++ apps/profiles/templatetags/profile_tags.py
@@ -1,12 +1,13 @@
 from django import template
 from apps.plus_permissions.api import secure_wrap, TemplateSecureWrapper
 from apps.plus_lib.utils import main_hub_name
+
 register = template.Library()
 
 def show_profile(request, user):
     homehub = user.homeplace.tggroup_set.filter(level='member')[0]
     profile = TemplateSecureWrapper(secure_wrap(user.get_profile(), request.user, interface_names=['Viewer']))
-    return {"user": user, "homehub":homehub, "profile":profile,"main_hub_name":main_hub_name()}
+    return {"user": user, "homehub":homehub, "profile":profile, "main_hub_name":main_hub_name()}
 register.inclusion_tag("profile_item.html")(show_profile)
 
 def clear_search_url(request):
