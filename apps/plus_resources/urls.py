9e06bebf367054496749af3ea0fb803d27f518b0
phil <phil.jones@the-hub.net>
2009-10-09 06:02:28 +0200

    url for Resource view

--- apps/plus_resources/urls.py
+++ apps/plus_resources/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 
     url(r'^(?P<resource_name>.+)/edit_resource/$', 'apps.plus_resources.views.edit_resource', name='edit_Resource'),
-    #url(r'^(?P<resource_name>.+)/view_resource/$', 'apps.plus_resources.views.view_resource', name='view_Resource'),
+    url(r'^(?P<resource_name>.+)/view_resource/$', 'apps.plus_resources.views.view_resource', name='view_Resource'),
 )
 
