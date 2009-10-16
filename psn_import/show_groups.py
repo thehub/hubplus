c368481c71b88d401bfebff0290b486489b6eab7
phil <phil.jones@the-hub.net>
2009-10-16 10:14:00 +0200

    wip

--- psn_import/show_groups.py
+++ psn_import/show_groups.py
@@ -2,4 +2,4 @@
 from apps.plus_groups.models import TgGroup
 
 for g in TgGroup.objects.all() :
-    print (g.group_name, g.place.name, g.group_type)
+    print (g.group_name, g.place.name, g.group_type, g.level)
