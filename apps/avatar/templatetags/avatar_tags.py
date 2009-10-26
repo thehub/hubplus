c20f4a0472ef97e19424f3bd2b4444990d078858
phil <phil.jones@the-hub.net>
2009-10-26 04:20:57 +0100

    importing avatar fo rmerging

---
+++ apps/avatar/templatetags/avatar_tags.py
@@ -0,0 +1,61 @@
+import hashlib
+import urllib
+
+from django import template
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+
+from avatar import AVATAR_DEFAULT_URL, AVATAR_GRAVATAR_BACKUP
+
+register = template.Library()
+
+def avatar_url(user, size=80):
+    if not isinstance(user, User):
+        try:
+            user = User.objects.get(username=user)
+        except User.DoesNotExist:
+            return AVATAR_DEFAULT_URL
+    avatars = user.avatar_set.order_by('-date_uploaded')
+    primary = avatars.filter(primary=True)
+    if primary.count() > 0:
+        avatar = primary[0]
+    elif avatars.count() > 0:
+        avatar = avatars[0]
+    else:
+        avatar = None
+    if avatar is not None:
+        if not avatar.thumbnail_exists(size):
+            avatar.create_thumbnail(size)
+        return avatar.avatar_url(size)
+    else:
+        return "/site_media/images/member.jpg"
+        if AVATAR_GRAVATAR_BACKUP:
+            return "http://www.gravatar.com/avatar/%s/?%s" % (
+                hashlib.md5(user.email).hexdigest(),
+                urllib.urlencode({'s': str(size)}),)
+        else:
+            return AVATAR_DEFAULT_URL
+register.simple_tag(avatar_url)
+
+def avatar(user, size=80):
+    if not isinstance(user, User):
+        try:
+            user = User.objects.get(username=user)
+            alt = unicode(user)
+            url = avatar_url(user, size)
+        except User.DoesNotExist:
+            url = AVATAR_DEFAULT_URL
+            alt = _("Default Avatar")
+    else:
+        alt = unicode(user)
+        url = avatar_url(user, size)
+    return """<img src="%s" alt="%s" width="%s" height="%s" />""" % (url, alt,
+        size, size)
+register.simple_tag(avatar)
+
+def render_avatar(avatar, size=80):
+    if not avatar.thumbnail_exists(size):
+        avatar.create_thumbnail(size)
+    return """<img src="%s" alt="%s" width="%s" height="%s" />""" % (
+        avatar.avatar_url(size), str(avatar), size, size)
+register.simple_tag(render_avatar)
