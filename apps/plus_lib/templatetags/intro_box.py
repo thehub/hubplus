59f9edca976ca4c0ae52e4de7f736d8e42ea1bb1
phil <phil.jones@the-hub.net>
2009-10-31 09:35:40 +0100

    intro box templatetag

---
+++ apps/plus_lib/templatetags/intro_box.py
@@ -0,0 +1,18 @@
+from django.template import get_library, Library, InvalidTemplateLibrary
+from django.conf import settings
+register = Library()
+
+@register.inclusion_tag('plus_lib/intro_box.html',takes_context=True)
+def intro_box(context) :
+    path = context['request'].path
+    if 'members' in path :
+        box = 'profiles/clients/%s/intro_box.html' % (settings.PROJECT_THEME)
+    elif 'group' in path :
+        box = 'plus_groups/clients/%s/groups/intro_box.html' % (settings.PROJECT_THEME)
+    elif 'hub' in path or 'region' in path :
+        box = 'plus_groups/clients/%s/hubs/intro_box.html' % (settings.PROJECT_THEME)
+    elif 'explore' in path or 'resources' in path :
+        box = '%sclients/%s/intro_box.html' % (path,settings.PROJECT_THEME)
+    else :
+        box = '' 
+    return {'relative_intro_box' : box, 'path' : path}
