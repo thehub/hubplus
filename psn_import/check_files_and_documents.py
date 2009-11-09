89fe4b79694b4e67aacc234af47aeffda08439bc
phil <phil.jones@the-hub.net>
2009-11-09 16:59:41 +0100

    check_files_and_documents

---
+++ psn_import/check_files_and_documents.py
@@ -0,0 +1,18 @@
+from psn_import.utils import load_all, reverse, maps, get_group_for
+
+load_all()
+
+def file_name(file) :
+    return 
+
+def check_files(lst) :
+    for file in lst :
+        
+
+if __name__ == '__main__' :
+    check_files(maps['File'])
+    check_documents(maps['Document'])
+
+
+
+
