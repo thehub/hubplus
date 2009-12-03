332c043adde0644af95280709dc5902d810426a5
phil <phil.jones@the-hub.net>
2009-12-03 18:56:16 +0100

    new status ui

--- apps/microblogging/templatetags/microblogging_tags.py
+++ apps/microblogging/templatetags/microblogging_tags.py
@@ -1,6 +1,10 @@
 
 from django import template
-from microblogging.models import Tweet
+from microblogging.models import Tweet, TweetInstance
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+from django.template import defaultfilters
+
 
 register = template.Library()
 
@@ -21,3 +25,26 @@ def tweet_listing(context, tweets, prefix_sender, are_mine) :
 def sent_tweet_listing(context, user, prefix_sender, are_mine):
     tweets = Tweet.objects.filter(sender_id=user.pk)
     return tweet_listing(context, tweets, prefix_sender, are_mine)
+
+@register.inclusion_tag('microblogging/form.html', takes_context=True)
+def microblogging_form(context, sender, starts_hidden=False) :
+    sender_class = ContentType.objects.get_for_model(sender)
+    tweets = TweetInstance.objects.tweets_from(sender).order_by("-sent")
+    if tweets :
+        latest_status = tweets[0]
+        status_since = defaultfilters.timesince(latest_status.sent)
+        status_text = latest_status.text
+    else:
+        status_since = ''
+        status_text = ''
+
+    path = context['request'].path
+
+    return {
+        'sender':sender,
+        'path':path,
+        'status_text':status_text,
+        'status_since':status_since,
+        'sender_class':sender_class,
+        'starts_hidden':starts_hidden,
+        }
