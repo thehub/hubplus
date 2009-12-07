f87cf810b3994c061c0e41a3d63a28f768b509d7
phil <phil.jones@the-hub.net>
2009-12-08 00:13:19 +0100

    checking groups

--- scripts/patch_permissions.py
+++ scripts/patch_permissions.py
@@ -10,8 +10,23 @@ def change_user(user) :
     print sc.get_slider_level('User.Viewer'), sc.get_slider_level('Profile.EmailAddressViewer')
 
 
+def change_group(group) :
+    print group.group_name, group.get_ref().permission_prototype
+    sc = group.get_security_context()
+    
+    print sc.get_slider_level('TgGroup.Viewer'), sc.get_slider_level('WikiPage.Viewer')
+    sc.switch_permission_prototype(group.get_ref().permission_prototype)
+    print sc.get_slider_level('TgGroup.Viewer'), sc.get_slider_level('WikiPage.Viewer')
+
+import ipdb
 
 if __name__ == '__main__' :
     for u in User.objects.all() :
         if u.username in ['anon','admin'] : continue
-        change_user(u)
+        #change_user(u)
+    
+    for g in TgGroup.objects.all() :
+        try:
+            change_group(g)
+        except :
+            ipdb.set_trace()
