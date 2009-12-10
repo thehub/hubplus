b52a7931a09aa9876b9dae5283d81f1bc7c90a68
phil <phil.jones@the-hub.net>
2009-12-10 23:06:47 +0100

    comment avatar

--- apps/plus_comments/templatetags/comment_tree.py
+++ apps/plus_comments/templatetags/comment_tree.py
@@ -7,22 +7,21 @@ from django.conf import settings
 register = template.Library()
 
 @register.inclusion_tag('plus_comments/comment_tree.html', takes_context=True)
-
 def comment_tree(context, target, name) :
-
     if is_not_viewable(target,'comment') :
         can_comment = False
     else :
         can_comment = True
 
-
     if name == 'Group' :
         if target.get_inner().is_hub_type() :
             name = settings.HUB_NAME
 
+    user = context['request'].user
     return {
         'target':target,
         'target_type' : name,
         'can_comment':can_comment,
+        'user':user,
         }
 
