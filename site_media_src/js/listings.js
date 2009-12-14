67022a8ac89dd95902720f6a1c14fc0776df0da6
salfield <salfield@the-hub.net>
2009-12-14 04:47:59 +0000

    sort ordering js changes + db patch

--- site_media_src/js/listings.js
+++ site_media_src/js/listings.js
@@ -1,5 +1,13 @@
 var listing_ready = function () {
-    jq('.order_list').change(function () {
+    jq('.tabbed_content #resources form.order_list').live('submit', function () {
+	var form = jq(this); //.closest('form');
+	console.log(form.serialize())  ;
+	jq('.tabbed_content #resources').load(form.attr('action') + ' form', form.serialize());
+		console.log("here");
+	return false;
+    });
+    jq('.order_list').live('change', function () {
 	jq(this).submit();
+	return false;
     });
 };
