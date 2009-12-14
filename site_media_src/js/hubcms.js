5003e82e64525593993b4fa4441784e97bccac1b
phil <phil.jones@the-hub.net>
2009-12-14 17:09:38 +0100

    don't show map when no location

--- site_media_src/js/hubcms.js
+++ site_media_src/js/hubcms.js
@@ -720,12 +720,15 @@ var plot_point = function(map, point) {
 var create_map = function (map_ele, callback) {
     if (GBrowserIsCompatible()) {
 	var location_str = map_ele.html();
-	map_ele.html("");
-	var map = new GMap2(map_ele.get(0));
-	var geocoder = new GClientGeocoder();
-	geocoder.getLatLng(location_str, function (point) {
-	    callback(map, point);
-	});
+	if (location_str) {
+	    
+	    map_ele.html("");
+	    var map = new GMap2(map_ele.get(0));
+	    var geocoder = new GClientGeocoder();
+	    geocoder.getLatLng(location_str, function (point) {
+				   callback(map, point);
+			       });
+	}
     }
 };
 
