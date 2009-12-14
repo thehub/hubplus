e53547531adb27f6fc9a93d5d1cb416f07e7bf69
salfield <salfield@the-hub.net>
2009-12-14 04:56:18 +0000

    sort ordering js changes remove console.log statement

--- site_media_src/js/listings.js
+++ site_media_src/js/listings.js
@@ -1,7 +1,6 @@
 var listing_ready = function () {
     jq('.tabbed_content #resources form.order_list').live('submit', function () {
 	var form = jq(this); //.closest('form');
-	console.log(form.serialize())  ;
 	jq('.tabbed_content #resources').load(form.attr('action') + ' form', form.serialize());
 	return false;
     });
