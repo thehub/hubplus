52a515feb7d0bd68727abc0ce79ba81569df0651
Admin Tom Salfield <admin-ts@mhpss-app.nile.the-hub.net>
2009-12-18 20:54:24 +0000

    link styling and resources page styling

--- site_media_src/js/plus_resources/replace_file.js
+++ site_media_src/js/plus_resources/replace_file.js
@@ -3,9 +3,11 @@ var replace_file = function () {
     upload_field.find('.keep_existing').click(function () {
 	upload_field.find('.replace_file, .keep_existing').hide();
 	upload_field.find('.replace_file_button, .file_name').show();
+	return false;
     });
     upload_field.find('.replace_file_button').click(function () {
 	upload_field.find('.replace_file, .keep_existing').show();
 	upload_field.find('.replace_file_button, .file_name').hide();
+	return false;
     });
 };
