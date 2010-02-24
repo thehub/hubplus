908277fcc8ab117230be0b51104e90fdede11b88
Phil Jones <admin-pj@plusdev-app.nile.the-hub.net>
2010-02-24 13:03:21 +0000

    creating default site_media directories ... note, need to replace with symlinks if that's what you want

--- on_clone/RUNME.sh
+++ on_clone/RUNME.sh
@@ -2,3 +2,18 @@
 
 cp on_clone/post-merge .git/hooks/
 
+ensure() {
+   if [ ! -d "$1" ]; then
+      mkdir "$1";
+   fi;
+}
+
+
+ensure 'site_media'
+cd site_media
+ensure 'css'
+ensure 'js'
+ensure 'themes'
+ensure 'images'
+ensure 'member_res' 
+ensure 'avatars'
