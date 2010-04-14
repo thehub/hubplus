1ef1b25ee916f17db87e58839148337954fb7115
Phil Jones <admin-pj@mhpss-app.nile.the-hub.net>
2010-04-14 16:14:40 +0000

    tweak

--- plus_import/avatars.py
+++ plus_import/avatars.py
@@ -9,6 +9,7 @@ from django.core.files.images import ImageFile
 from apps.plus_groups.models import Location
 
 for u in User.objects.all() :
+
     f_name = 'hubplus_avatars/binaries/user-%s'%u.id
     try:
         f = ImageFile(open(f_name),'rb')
@@ -20,14 +21,26 @@ for u in User.objects.all() :
 
     path = avatar_file_path(target=u, filename=u.username)
  
-    avatar = Avatar(
+    new_avatar = Avatar(
         target = u.get_ref(),
         primary = True,
         avatar = path,
         )
 
-    avatar.save()
-    new_file = avatar.avatar.storage.save(path, f)
-    avatar.save()
+    avatars = Avatar.objects.filter(target=u).order_by('-primary')
+ 
+    if avatars :    
+        old_avatar = avatars[0]
+        #if old_avatar.avatar.size == new_avatar.avatar.size :
+        #    print "exists"
+        #    continue
+
+    #import ipdb
+    #ipdb.set_trace()
+    print avatars, new_avatar
+
+    new_avatar.save()
+    new_file = new_avatar.avatar.storage.save(path, f)
+    new_avatar.save()
    
 
