8aed11bca58ce636819d2705685a87432bfa4a34
phil <phil@hanbury.(none)>
2010-06-18 13:04:40 +0100

    script to view which hosting groups, all_members_hosts isn't a member of

---
+++ scripts/ensure_site_admin_powers.py
@@ -0,0 +1,13 @@
+
+from apps.plus_groups.models import TgGroup
+
+from apps.plus_permissions.default_agents import get_all_members_group
+
+all_members = get_all_members_group()
+site_admin = all_members.get_admin_group()
+
+for g in TgGroup.objects.hubs().all() :
+    hosts = g.get_admin_group()
+    if site_admin not in hosts.get_members() :
+        print g
+        
